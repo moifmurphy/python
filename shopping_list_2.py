01538310bff8a96a9662b285f06f4f5b1fb35906
Moif Murphy <moifmurphy@googlemail.com>
2017-04-28 11:25:48 +0100

    Added main function.

--- shopping_list_2.py
+++ shopping_list_2.py
@@ -21,22 +21,24 @@ def add_to_list(new_item):
     # add new items to our list
     shopping_list.append(new_item)
     print("Added {}. list now {} items.".format(new_item, len(shopping_list)))
-    
-show_help()
 
-while True:
-    # ask for new items
-    new_item = input("> ")
+def main():
     
-    # be able to quit the app
-    if new_item == 'DONE':
-        break
-    elif new_item == 'HELP':
-        show_help()
-        continue
-    elif new_item == 'SHOW':
-        show_list()
-        continue
-    add_to_list(new_item)
-          
-show_list()
+    show_help()
+
+    while True:
+        # ask for new items
+        new_item = input("> ")
+        
+        # be able to quit the app
+        if new_item == 'DONE':
+            break
+        elif new_item == 'HELP':
+            show_help()
+            continue
+        elif new_item == 'SHOW':
+            show_list()
+            continue
+        add_to_list(new_item)
+              
+    show_list()
